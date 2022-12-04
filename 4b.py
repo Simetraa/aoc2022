13254f615d7c0c0170f1b69c5dee50b5c1510626
Simetraa <simetra@simetra.dev>
2022-12-04 23:26:21 +0000

    Day 4

---
+++ 4b.py
@@ -0,0 +1,18 @@
+from aocd import data, submit
+
+lines = data.splitlines()
+
+count = 0
+for line in lines:
+    a, b = line.split(",")
+    a, b = a.split("-"), b.split("-")
+    a = range(int(a[0]), int(a[1]) + 1)
+    b = range(int(b[0]), int(b[1]) + 1) # increment by one as stop is omitted from the resulting range
+
+    a = set(a)
+    b = set(b)
+
+    if a & b:
+        count += 1
+
+submit(count)
